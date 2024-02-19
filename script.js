a0380302621b4c74f416fe5c15b3272429a65193
Fátima <fatimaalvesa10@gmail.com>
2024-02-19 10:09:43 +0000

    fixed js and other stuff

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-window.onload = function () {
+window.addEventListener("load", function () {
   document.getElementById("button").addEventListener("click", function () {
     var valNum = document.getElementById("heightInput").value;
     LengthConverter(valNum);
   });
-};
+
+  document.getElementById("close").addEventListener("click", function () {
+    document.getElementById("modal").classList.add("hidden");
+    document.getElementById("modal").classList.remove("visible");
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == document.getElementById("modal")) {
+      document.getElementById("modal").classList.add("hidden");
+      document.getElementById("modal").classList.remove("visible");
+    }
+  });
+});
 
 function LengthConverter(valNum) {
   var outputMeters = valNum / 100;
 
   document.getElementById("modalContent").innerHTML =
     "<p>Your height in meters is: " + outputMeters + "m</p>";
-    
-  document.getElementById("modal").style.display = "block";
+
+  document.getElementById("modal").classList.remove("hidden");
+  document.getElementById("modal").classList.add("visible");
 }
-
-document.getElementById("close").onclick = function () {
-  document.getElementById("modal").style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == document.getElementById("modal")) {
-    document.getElementById("modal").style.display = "none";
-  }
-};
